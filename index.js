b96bf856209417839110deb2fad958dfad403c3c
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-01 08:27:13 +0100

    Allow use of auth and post routes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 const port = process.env.PORT || 3000;
 
