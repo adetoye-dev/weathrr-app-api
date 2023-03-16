32362b5804f6582a60105c0dfc949bf565bc1c30
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 11:39:28 +0100

    Add route to get a user by Id

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../handlers/users.js";
+import { getUsers, getUser } from "../handlers/users.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:userId", getUser);
 
 export default router;
