3136e739c29d61a8b2817cb83fa2eb6ce9696e65
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-16 09:43:22 +0100

    Fetch user from database

--- handlers/users.js
+++ handlers/users.js
@@ -1,3 +1,16 @@
+import { db } from "../connection.js";
+
 export const getUsers = (req, res) => {
   res.send("Hello from server router");
+};
+
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+
+  const q = `SELECT id , name , profilePic, city FROM users  
+      WHERE (id = ?)`;
+  db.query(q, [userId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
 };
