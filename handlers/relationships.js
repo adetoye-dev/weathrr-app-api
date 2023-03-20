701ce355e183dedd98979c9c5186eaf292745806
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-20 07:17:43 +0100

    Create add and remove relationships query

--- handlers/relationships.js
+++ handlers/relationships.js
@@ -21,6 +21,39 @@ export const getUserFollowings = (req, res) => {
   });
 };
 
-export const addRelationship = (req, res) => {};
+export const addRelationship = (req, res) => {
+  const userId = req.body.userId;
 
-export const deleteRelationship = (req, res) => {};
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("User not logged in");
+
+  jwt.verify(token, "secretkey", (err, data) => {
+    if (err) return res.status(403).json("Invalid Token");
+    const q =
+      "INSERT INTO relationships (`followerUserId`, `followedUserId`) VALUES (?)";
+    const values = [data.id, userId];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post added to bookmarks");
+    });
+  });
+};
+
+export const deleteRelationship = (req, res) => {
+  const userId = req.query.userId;
+
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("User not logged in");
+
+  jwt.verify(token, "secretkey", (err, data) => {
+    if (err) return res.status(403).json("Invalid Token");
+    const q =
+      "DELETE FROM relationships WHERE `followerUserId` = ? AND `followedUserId` = ?";
+
+    db.query(q, [data.id, userId], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post removed from bookmarks");
+    });
+  });
+};
