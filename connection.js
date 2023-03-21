5bea7bbeb8815bfae4115a53aa731bb63ff35dae
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-21 20:26:51 +0100

    Use live db connection

--- connection.js
+++ connection.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
 
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "MySQLadmin_12345",
-  database: "weathrr",
-});
+dotenv.config();
+
+export const db = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+// db.end();
