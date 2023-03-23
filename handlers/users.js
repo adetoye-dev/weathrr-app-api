3bec5e5ce464c11f16a508b456c7053dbba8c373
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-23 17:18:49 +0100

    Update user fetch

--- handlers/users.js
+++ handlers/users.js
@@ -7,7 +7,7 @@ export const getUsers = (req, res) => {
 export const getUser = (req, res) => {
   const userId = req.params.userId;
 
-  const q = `SELECT id , name , profilePic, city FROM users  
+  const q = `SELECT id , name , profilePic, city, about FROM users  
       WHERE (id = ?)`;
   db.query(q, [userId], (err, data) => {
     if (err) return res.status(500).json(err);
