85b8541ba01988fe7316f77a334139d639e48350
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-26 19:16:05 +0100

    Create route for nearby posts fetch

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ import {
   getFollowedUsersPosts,
   addPost,
   getRecommendedPosts,
+  getNearbyPosts,
 } from "../handlers/posts.js";
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.get("/following", getFollowedUsersPosts);
 router.get("/:userId", getUserPosts);
 router.post("/addPost", addPost);
 router.post("/recommend", getRecommendedPosts);
+router.post("/region", getNearbyPosts);
 
 export default router;
