de7fd16ed5d3d8dac8479295a090222ac8196d93
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-30 10:31:48 +0100

    Refactor userUpdate handler

--- handlers/users.js
+++ handlers/users.js
@@ -1,6 +1,5 @@
 import { db } from "../connection.js";
 import jwt from "jsonwebtoken";
-import axios from "axios";
 
 export const getUser = (req, res) => {
   const userId = req.params.userId;
@@ -19,36 +18,20 @@ export const updateUser = (req, res) => {
 
   jwt.verify(token, "secretkey", (err, data) => {
     if (err) return res.status(403).json("Invalid Token");
-    saveUserData();
 
-    async function saveUserData() {
-      await axios
-        .post(
-          "https://www.filestackapi.com/api/store/S3?key=" +
-            process.env.FILESTACK_API_KEY,
-          req.files.profilePic.data,
-          {
-            headers: {
-              "Content-Type": "image/png",
-            },
-          }
-        )
-        .then((res) => res.data)
-        .then((img) => {
-          const q =
-            "UPDATE users SET `name` = ?, `profilePic` = ?, `city` = ?, `about` = ? WHERE (id = ?)";
-          const values = [
-            req.body.name,
-            img.url,
-            req.body.city,
-            req.body.about,
-            data.id,
-          ];
-          db.query(q, [...values], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("Profile Updated Successfully!!");
-          });
-        });
-    }
+    const q =
+      "UPDATE users SET `name` = ?, `profilePic` = ?, `picId` = ?, `city` = ?, `about` = ? WHERE (id = ?)";
+    const values = [
+      req.body.name,
+      req.body.img.url,
+      req.body.img.id,
+      req.body.city,
+      req.body.about,
+      data.id,
+    ];
+    db.query(q, [...values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Profile Updated Successfully!!");
+    });
   });
 };
