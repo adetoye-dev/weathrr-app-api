9f3c7626ca3a5dcd1ea3eb1b551da2fafb54533c
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-30 10:03:48 +0100

    Refactor uploads handler to use cloudinary Refactor uploads handler to use cloudinary for storing posts and user images

--- handlers/uploads.js
+++ handlers/uploads.js
@@ -1,29 +1,45 @@
 import dotenv from "dotenv";
-import path from "path";
-const __dirname = path.resolve();
+import * as cloudinary from "cloudinary";
 
 dotenv.config();
 
-export const uploadPostImage = (req, res) => {
-  const filename = Date.now() + "_" + req.files.img.name;
-  const file = req.files.img;
-  const uploadPath = __dirname + "/uploads/postImages/" + filename;
-  file.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-  });
-  res.status(200).json(filename);
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadPostImage = async (req, res) => {
+  const file = req.body.img;
+
+  // Upload
+  const data = await cloudinary.v2.uploader
+    .upload(file, {
+      folder: "weatherPosts",
+    })
+    .then((data) => data)
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+
+  res.status(200).json({ id: data.public_id, url: data.url });
+  console.log(data);
 };
 
-export const uploadProfileImage = (req, res) => {
-  const filename = Date.now() + "_" + req.files.img.name;
-  const file = req.files.img;
-  const uploadPath = __dirname + "/uploads/profileImages/" + filename;
-  file.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-  });
-  res.status(200).json("File Uploaded Successfully!");
-};
+export async function uploadProfileImage(req, res) {
+  const file = req.body.img;
+
+  // Upload
+  const data = await cloudinary.v2.uploader
+    .upload(file, {
+      folder: "weatherUsers",
+    })
+    .then((data) => data)
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+
+  res.status(200).json({ id: data.public_id, url: data.secure_url });
+  console.log(data);
+}
