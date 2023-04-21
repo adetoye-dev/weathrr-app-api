c35d2a745e56dff29ffcd3ea48cbbb3ee21d2876
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-21 22:29:00 +0100

    Create google authentication and callback routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
 import express from "express";
 import { register, login, logout } from "../handlers/auth.js";
+import passport from "passport";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/google", passport.authenticate("google"));
+router.get(
+  "/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.send("Hello, you made it to the callback");
+  }
+  // passport.authenticate("google", {
+  //   successReturnToOrRedirect: "http://localhost:3000/hello",
+  //   failureRedirect: "http://localhost:3000/",
+  // })
+);
 
 export default router;
