8e9b77b23a2d8f342c3a91d14007146d18693839
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-22 14:56:41 +0100

    Handle passport callback serialization

--- config/passport-config.js
+++ config/passport-config.js
@@ -14,14 +14,17 @@ passport.use(
     },
     (request, accessToken, refreshToken, profile, done) => {
       console.log("Yay! You made it to the callbacks.");
-      console.log(profile);
 
       //CHECK IF USER ALREADY EXISTS
       const q = "SELECT * FROM `google_users` where googleId = ?";
       db.query(q, [profile.id], (err, data) => {
         // if (err) return res.status(500).json(err);
         if (err) return console.log(err);
-        if (data.length) return console.log("User already exists!");
+        if (data.length) {
+          const { ...user } = data[0];
+          console.log("User already exists:", user);
+          return done(null, user);
+        }
 
         console.log("This is a new user");
         //CREATE NEW USER
@@ -36,9 +39,37 @@ passport.use(
         ];
         db.query(q, [values], (err, data) => {
           if (err) return res.status(500).json(err);
-          console.log("Created new user:", values);
+          const q = "SELECT * FROM `google_users` where googleId = ?";
+          db.query(q, [profile.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            // return res.status(200).json(data);
+            const { ...user } = data[0];
+            console.log("Created new user: ", user);
+            return done(null, data);
+          });
         });
       });
     }
   )
 );
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function (user, done) {
+  process.nextTick(function () {
+    done(null, {
+      id: user.id,
+      googleId: user.googleId,
+      name: user.name,
+      profilePic: user.profilePic,
+      city: user.city,
+      about: user.about,
+      channel: user.channel,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, done) {
+  process.nextTick(function () {
+    return done(null, user);
+  });
+});
