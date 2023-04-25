f0c678087b33b4080ae152780ba14e3849292a62
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-04-25 13:44:15 +0100

    Fix db connection error

--- connection.js
+++ connection.js
@@ -3,13 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const db = mysql.createConnection(process.env.DATABASE_URL);
-// console.log("Connected to PlanetScale!");
-// db.end();
-
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "MySQLadmin_12345",
-  database: "weathrr",
-});
+export const db = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+db.end();
